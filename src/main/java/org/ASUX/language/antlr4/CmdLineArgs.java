da9b400df43a5a50c4a4a702d66d9425bee1f1a0
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2020-04-01 23:02:50 -0400

    ADD: Added a new method copyBasicFlags()

--- src/main/java/org/ASUX/language/antlr4/CmdLineArgs.java
+++ src/main/java/org/ASUX/language/antlr4/CmdLineArgs.java
@@ -5,4 +5,17 @@ package org.ASUX.language.antlr4;
  * The Command parsers for YAML, AWS, etc.. will have classes implementing this interface.
  */
 public interface CmdLineArgs {
+
+
+    //=================================================================================
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    //=================================================================================
+
+    /**
+     * Given the original object of this class, copy these attributes to the 2nd object of this class.  Classes implementing this interface will know what instance-variables to copy.
+     * @param _orig a NotNull reference
+     * @param _copy a NotNull reference
+     */
+    public void copyBasicFlags( final CmdLineArgs _orig, final CmdLineArgs _copy );
+
 }
