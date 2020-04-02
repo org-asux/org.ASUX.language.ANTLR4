4cb23a8d51ba9f8cde1383084599f993d085ab68
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2020-04-01 21:15:05 -0400

    ADD: new simple interface for use by Projects implementing ANTLR4 parsing

---
+++ src/main/java/org/ASUX/language/antlr4/CmdLineArgs.java
@@ -0,0 +1,8 @@
+package org.ASUX.language.antlr4;
+
+/**
+ * This empty-interface is a placeholder, with the __BIG__ assumption that its better that using java.lang.Object as the return value of {@link GenericCmdANTLR4Parser#parseYamlCommandLine(String)}.
+ * The Command parsers for YAML, AWS, etc.. will have classes implementing this interface.
+ */
+public interface CmdLineArgs {
+}
